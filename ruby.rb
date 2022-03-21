871058e4b759baf1d48260ba2c54120dfd8502d1
LawrenceDeacon <lawdeacon.ld@gmail.com>
2022-03-21 21:09:23 +0000

    Created Board class

--- ruby.rb
+++ ruby.rb
@@ -11,8 +11,19 @@ module Components
         end
     end
 
+    class Board
+        def initialize
+            @square = "| |"
+            @row = [@square, @square, @square, "\n"].join
+            @board = [@row,@row,@row]
+            puts @board
+        end
+    end
+
 end
 
+# Init
 player = Components::Players
 player1 = player.new("Lawrence", "X")
 player2 = player.new("Mike", "O")
+board = Components::Board.new
