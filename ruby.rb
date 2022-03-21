6567da3704215dbfb1e6974436fedf39f88197d8
LawrenceDeacon <lawdeacon.ld@gmail.com>
2022-03-21 21:05:27 +0000

    Removed Board Class and MakeMove class

--- ruby.rb
+++ ruby.rb
@@ -11,45 +11,8 @@ module Components
         end
     end
 
-    # Board Class
-    class Board
-        @@board = []
-        def initialize
-            @square = ["|"," ","|"].join
-            @row = [@square,@square,@square, "\n"].join
-            @board = [@row,@row,@row]
-            puts @board
-        end
-    end
-
-    # Move Class
-    class MakeMove
-        @@moves = []
-        attr_reader :show_moves
-        def initialize(player, board, row, square)
-            @player = player
-            @board = board
-            @row = row
-            @square = square
-            @@moves = [@player,@board,@row,@square]
-        end
-
-        def show_moves
-            p "show_moves"
-            return @@moves
-        end
-    end
-
 end
 
 player = Components::Players
 player1 = player.new("Lawrence", "X")
 player2 = player.new("Mike", "O")
-
-board = Components::Board.new
-move = Components::MakeMove
-
-p player.players
-
-# Move written as move[row,square]
-player1.make_move(1,1)
